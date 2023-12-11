ef19a978a332f6cb741f28bbaa8f557199cd857a
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-12-12 01:35:51 +0700

    fix: fix clientID

--- app/src/main/java/com/example/kotlin_tracktic/util/SharedViewModel.kt
+++ app/src/main/java/com/example/kotlin_tracktic/util/SharedViewModel.kt
@@ -4,15 +4,23 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.res.TypedArrayUtils.getString
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.kotlin_tracktic_theincredibles.R
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class SharedViewModel(): ViewModel() {
+
+    private lateinit var auth: FirebaseAuth
+
 
     @SuppressLint("SuspiciousIndentation")
     fun saveData(
@@ -71,12 +79,13 @@ class SharedViewModel(): ViewModel() {
         navController: NavController,
         backToMainScreen: () -> Unit
     ) = CoroutineScope(Dispatchers.IO).launch{
-        val firestoreRef = Firebase.firestore
-            .collection("transactions")
-            .document(id)
+
+        val db = Firebase.firestore
 
         try {
-            firestoreRef.delete()
+            db.collection("transactions")
+                .document(id)
+                .delete()
                 .addOnSuccessListener {
                     Toast.makeText(context, "Data saved", Toast.LENGTH_SHORT).show()
                     navController.popBackStack()
@@ -86,8 +95,66 @@ class SharedViewModel(): ViewModel() {
         }
     }
 
-    fun printData(){
-        Log.d("TransactionScreen", "Submitted!")
+    fun signUp(
+        email: String,
+        password: String,
+        context: Context,
+        navController: NavController,
+        backToMainScreen: () -> Unit
+    ) = CoroutineScope(Dispatchers.IO).launch{
+//        auth = FirebaseAuth.getInstance()
+        auth = Firebase.auth
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Sign up success", Toast.LENGTH_SHORT).show()
+                    navController.popBackStack()
+                } else {
+                    Toast.makeText(context, "Sign up failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun signIn(
+        email: String,
+        password: String,
+        context: Context,
+        navController: NavController,
+        backToMainScreen: () -> Unit
+    ) = CoroutineScope(Dispatchers.IO).launch{
+//        auth = FirebaseAuth.getInstance()
+        auth = Firebase.auth
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Sign in success", Toast.LENGTH_SHORT).show()
+                    navController.popBackStack()
+                } else {
+                    Toast.makeText(context, "Sign in failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun signInWithGoogle(
+        context: Context,
+        navController: NavController,
+        backToMainScreen: () -> Unit
+    ) = CoroutineScope(Dispatchers.IO).launch{
+//        auth = FirebaseAuth.getInstance()
+        auth = Firebase.auth
+
+        BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId("610254144825-al0m2hm47d56djoiomtn5jcekm3i6g81.apps.googleusercontent.com")
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
     }
 
 }
