0c3b80342dfbe846aafd9c2b0615ec4d376cf5e0
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-12-21 18:25:25 +0700

    Add datepicker function

--- app/src/main/java/com/example/kotlin_tracktic/TransactionScreen.kt
+++ app/src/main/java/com/example/kotlin_tracktic/TransactionScreen.kt
@@ -1,9 +1,12 @@
 package com.example.kotlin_tracktic
 
+import android.widget.DatePicker
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,29 +21,39 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -50,8 +63,10 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.kotlin_tracktic.util.SharedViewModel
 import com.example.kotlin_tracktic.util.TransactionData
+import com.example.kotlin_tracktic_theincredibles.R
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import java.text.DateFormatSymbols
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -72,6 +87,7 @@ fun TransactionScreen(
     var remarksValue by remember { mutableStateOf("") }
     var categoryValue by remember { mutableStateOf("") }
     var typeValue by remember { mutableStateOf("") }
+    var endDate by rememberSaveable { mutableStateOf(Date().time) }
 
 
     val buttons = remember {
@@ -270,11 +286,9 @@ fun TransactionScreen(
                             onValueChange = {},
                             label = { Text("Type") },
                             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                            colors = ExposedDropdownMenuDefaults.textFieldColors(
-                                containerColor = Color.White,
-                                textColor = Color.Black,
-                                focusedIndicatorColor = Color.Transparent
-                            ),
+                            colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+                            textStyle = TextStyle(
+                                color = Color.Black),
                             shape = RoundedCornerShape(20),
                             modifier = Modifier
                                 .menuAnchor()
@@ -302,7 +316,7 @@ fun TransactionScreen(
 
                 // Transaction Category
                 Column(modifier = Modifier.padding(top = 10.dp)) {
-                    val options = listOf("Transportation", "Food", "Electronic", "Shop", "Others")
+                    val options = listOf("Transportation", "Food", "Shop", "Others")
                     var expanded by remember { mutableStateOf(false) }
                     var selectedOptionText by remember { mutableStateOf(options[0]) }
 
@@ -318,11 +332,9 @@ fun TransactionScreen(
                             onValueChange = {},
                             label = { Text("Category") },
                             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                            colors = ExposedDropdownMenuDefaults.textFieldColors(
-                                containerColor = Color.White,
-                                textColor = Color.Black,
-                                focusedIndicatorColor = Color.Transparent
-                            ),
+                            colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+                            textStyle = TextStyle(
+                                color = Color.Black),
                             shape = RoundedCornerShape(20),
                             modifier = Modifier
                                 .menuAnchor()
@@ -350,59 +362,9 @@ fun TransactionScreen(
 
                 // Date
                 Column(modifier = Modifier.padding(top = 10.dp)) {
-                    val options = listOf("21/05/2022", "Food", "Electronic", "Shop", "Others")
-                    var expanded by remember { mutableStateOf(false) }
-                    var selectedOptionText by remember { mutableStateOf(options[0]) }
-
-                    ExposedDropdownMenuBox(
-                        expanded = expanded,
-                        onExpandedChange = { expanded = !expanded },
-                    ) {
-                        TextField(
-                            readOnly = true,
-                            value = selectedOptionText,
-                            onValueChange = {},
-                            label = { Text("Date") },
-                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                            colors = ExposedDropdownMenuDefaults.textFieldColors(
-                                containerColor = Color.White,
-                                textColor = Color.Black,
-                                focusedIndicatorColor = Color.Transparent
-                            ),
-                            shape = RoundedCornerShape(20),
-                            modifier = Modifier
-                                .menuAnchor()
-                                .border(1.dp, Color.LightGray)
-                                .fillMaxWidth()
-                        )
-                        ExposedDropdownMenu(
-                            expanded = expanded,
-                            onDismissRequest = { expanded = false },
-                        ) {
-                            options.forEach { selectionOption ->
-                                DropdownMenuItem(
-                                    text = { Text(selectionOption) },
-                                    onClick = {
-                                        selectedOptionText = selectionOption
-                                        expanded = false
-                                    },
-                                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                                )
-                            }
-                        }
+                    EndDateTextField { endDate = it }
                     }
-                }
 
-//            Column() {
-//                AndroidView(
-//                    { CalendarView(it) },
-//                    modifier = Modifier.wrapContentWidth(),
-//                    update = { views ->
-//                        views.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
-//                        }
-//                    }
-//                )
-//            }
 
                 // Remarks
                 Column(modifier = Modifier.padding(top = 10.dp)) {
@@ -471,6 +433,141 @@ fun TransactionScreen(
     }
 }}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EndDateTextField(endDate: (Long) -> Unit) {
+
+    var shouldDisplay by remember { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed: Boolean by interactionSource.collectIsPressedAsState()
+    if (isPressed) {
+        shouldDisplay = true
+    }
+
+    val today = Calendar.getInstance()
+    today.set(Calendar.HOUR_OF_DAY, 0)
+    today.set(Calendar.MINUTE, 0)
+    today.set(Calendar.SECOND, 0)
+    today.set(Calendar.MILLISECOND, 0)
+    val currentDayMillis = today.timeInMillis
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = System.currentTimeMillis(),
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return utcTimeMillis >= currentDayMillis
+            }
+        }
+    )
+
+    var selectedDate by rememberSaveable {
+        mutableStateOf(
+            datePickerState.selectedDateMillis?.toFormattedDateString() ?: ""
+        )
+    }
+
+    EndDatePickerDialog(
+        state = datePickerState,
+        shouldDisplay = shouldDisplay,
+        onConfirmClicked = { selectedDateInMillis ->
+            selectedDate = selectedDateInMillis.toFormattedDateString()
+            endDate(selectedDateInMillis)
+        },
+        dismissRequest = {
+            shouldDisplay = false
+        }
+    )
+
+    TextField(
+        readOnly = true,
+        value = selectedDate,
+        onValueChange = {},
+        trailingIcon = { Icons.Default.DateRange },
+        interactionSource = interactionSource,
+        label = { Text("Date") },
+        colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
+        textStyle = TextStyle(
+            color = Color.Black),
+        shape = RoundedCornerShape(20),
+        modifier = Modifier
+            .background(Color.White)
+            .border(1.dp, Color.LightGray)
+            .fillMaxWidth()
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EndDatePickerDialog(
+    state: DatePickerState,
+    shouldDisplay: Boolean,
+    onConfirmClicked: (selectedDateInMillis: Long) -> Unit,
+    dismissRequest: () -> Unit
+) {
+    if (shouldDisplay) {
+        DatePickerDialog(
+            onDismissRequest = dismissRequest,
+            confirmButton = {
+                Button(
+                    modifier = Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp),
+                    onClick = {
+                        state.selectedDateMillis?.let {
+                            onConfirmClicked(it)
+                        }
+                        dismissRequest()
+                    }
+                ) {
+                    Text(text = "ok")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = dismissRequest) {
+                    Text(text = "cancel")
+                }
+            },
+            content = {
+                DatePicker(
+                    state = state,
+                    showModeToggle = false,
+                    headline = {
+                        state.selectedDateMillis?.toFormattedDateString()?.let {
+                            Text(
+                                modifier = Modifier.padding(start = 16.dp),
+                                text = it
+                            )
+                        }
+                    }
+                )
+            }
+        )
+    }
+}
+
+fun Date.toFormattedDateString(): String {
+    val sdf = SimpleDateFormat("EEEE, LLLL dd", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Date.toFormattedMonthDateString(): String {
+    val sdf = SimpleDateFormat("MMMM dd", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Date.toFormattedDateShortString(): String {
+    val sdf = SimpleDateFormat("dd", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Long.toFormattedDateString(): String {
+    val sdf = SimpleDateFormat("LLLL dd, yyyy", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Date.hasPassed(): Boolean {
+    val calendar = Calendar.getInstance()
+    calendar.add(Calendar.SECOND, -1)
+    val oneSecondAgo = calendar.time
+    return time < oneSecondAgo.time
+}
 @Preview
 @Composable
 fun TransactionScreenPreview() {
